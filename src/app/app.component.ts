3e81c1f63bae7ce7e5e75e39f1814a90c9a807a9
Mangonia <rauleduardo16@hotmail.com>
2019-08-07 13:39:29 -0500

    installe websocket hay un problema en las dependencias que no supe arreglar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import Ws from '@adonisjs/websocket-client'
+const ws = Ws('ws://localhost:3333')
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cliente';
+  constructor(){
+    const socket = ws.subscribe('socket');
+    socket.on('ready', () => {
+      console.log("conectado")
+    })
+  }
 }
+ 

51d6b51574bcda31f58bf3e223afb25db531c9ea
dianachaparro1 <diana.chaparro@hotmail.com>
2019-08-09 14:23:04 -0500

    Diana vistas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProveedorRegistroComponent } from './coponentes/proveedor-registro/proveedor-registro.component';
+import { ConceptoRegistroComponent } from './coponentes/concepto-registro/concepto-registro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProveedorRegistroComponent,
+    ConceptoRegistroComponent
   ],
   imports: [
     BrowserModule

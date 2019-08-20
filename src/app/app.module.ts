d44481e16a3043be09e8f3a117dc6d43fab38d48
raul <rauleduardo16@hotmail.com>
2019-08-20 15:20:02 -0500

    bajando cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ProveedorRegistroComponent } from './coponentes/proveedor-registro/proveedor-registro.component';
 import { ConceptoRegistroComponent } from './coponentes/concepto-registro/concepto-registro.component';
+import { VentaComponent } from './coponentes/venta/venta.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProveedorRegistroComponent,
-    ConceptoRegistroComponent
+    ConceptoRegistroComponent,
+    VentaComponent
   ],
   imports: [
     BrowserModule

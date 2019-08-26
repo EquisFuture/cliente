dec1c5678e38518d9a75e584caf9387464a0dfbf
EquisFuture <xcaritow@gmail.com>
2019-08-26 17:04:44 -0500

    actualizacion: edicion de usuarios

--- src/app/servicios/usuarios.service.ts
+++ src/app/servicios/usuarios.service.ts
@@ -1,19 +1,73 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders, HttpErrorResponse} from '@angular/common/http';
 import { Usuario } from '../modelos/Usuario';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
+// la variable ws la ruta para conectarse al socket
+import Ws from '@adonisjs/websocket-client';
+const ws = Ws('ws://localhost:3333');
 
 @Injectable({
 providedIn: "root"
 })
 export class UsuariosService {
 
+  // aqui se guarda el usuario a editar
+  private usuario = new BehaviorSubject([]);
+  usuario_editar = this.usuario.asObservable();
+
+  // aqui se guarda la lista de usuarios
+  private usuarios = new BehaviorSubject([]);
+  lista_usuarios = this.usuarios.asObservable();
+
   url: string = 'http://127.0.0.1:3333/';
   constructor(private request: HttpClient) { }
 
-  registrarUsuario(json: any){
+  conectar(){
+    try{
+      // generamos la conexión al socket
+      ws.connect();
+      const canal = ws.subscribe('usuarios');
+
+      // éste método se ejecutará cuando la conexión al canal usuarios esté lista.
+      canal.on('ready', () => {
+        console.log('** suscripción usuarios lista');
+      })
+      // cuando reciba una actualización, lo mandará a la lista de usuarios
+      canal.on('actualizar', (usuarios) => {
+        console.log("** usuarios recibido del ws")
+        this.actualizarUsuarios(usuarios);
+      })
+    }
+    catch(e){console.log('ya está conectado')}
+  }
+
+  cerrarConexion(){
+    try{
+      ws.getSubscription('usuarios').close();
+      console.log('** desconectado de usuarios')
+    }
+    catch(e){console.log('no hay conexion para cerrar')}
+  }
+
+  enviarUsuarios(usuarios){
+    try{
+      ws.getSubscription('usuarios').emit('actualizar', usuarios);
+    }catch(e){ console.log(e); }
+  }
+
+  // actualiza la tabla de usuarios
+  actualizarUsuarios(usuarios){
+    this.usuarios.next(usuarios);
+  }
+
+  // actualiza el usuario a editar
+  actualizarUsuario(usuario){
+    this.usuario.next(usuario);
+  }
+
+  registrarUsuario(usuario: any){
     let headers = new HttpHeaders().set('Content-Type','application/json').set('auth',localStorage.getItem('token'));
-    return this.request.post(this.url + 'registrar-usuario', json, {headers:headers});
+    return this.request.post(this.url + 'registrar-usuario', usuario, {headers:headers});
   }
 
   iniciarSesion(email: string, password: string){
@@ -24,6 +78,12 @@ export class UsuariosService {
   obtenerUsuarios(): Observable<Usuario[]>{
     let headers = new HttpHeaders().set('Content-Type','application/json').set('auth',localStorage.getItem('token'));
     return this.request.get<Usuario[]>(this.url + 'obtener-usuarios', {headers:headers});
+  }
+
+  editarUsuario(usuario: any){
+    console.log(usuario)
+    let headers = new HttpHeaders().set('Content-Type','application/json').set('auth',localStorage.getItem('token'));
+    return this.request.post(this.url + 'editar-usuario', usuario, {headers:headers});
   }
 }
 ;

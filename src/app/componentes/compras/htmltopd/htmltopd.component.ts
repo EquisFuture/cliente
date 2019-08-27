fa8cb48620a1ae05004c1a538a71309d9b2b1f43
jaimeglz <jaime.glz.hdz.28@gmail.com>
2019-08-27 15:59:50 -0500

    actualizacion: compras pdf arreglos

---
+++ src/app/componentes/compras/htmltopd/htmltopd.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-htmltopd',
+  templateUrl: './htmltopd.component.html',
+  styleUrls: ['./htmltopd.component.css']
+})
+export class HtmltopdComponent implements OnInit {
+
+  
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  //   public captureScreen()  
+  // {  
+  //   var data = document.getElementById('detalles');  
+  //   html2canvas(data).then(canvas => {  
+  //     // Few necessary setting options  
+  //     var imgWidth = 208;   
+  //     var pageHeight = 295;    
+  //     var imgHeight = canvas.height * imgWidth / canvas.width;  
+  //     var heightLeft = imgHeight;  
+  
+  //     const contentDataURL = canvas.toDataURL('image/png')  
+  //     let pdf = new jspdf('p', 'mm', 'a4'); // A4 size page of PDF  
+  //     var position = 0;  
+  //     pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)  
+  //     pdf.save('MYPdf.pdf'); // Generated PDF   
+  //   });  
+  // }
+  
+    
+  }
+
+
